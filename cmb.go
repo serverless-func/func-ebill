c795a70f64e67f5dd4afbb1c6895e56534e96edc
dongfg <mail@dongfg.com>
2020-11-18 14:06:23 +0800

    feat: parse cmb file bill

--- cmb.go
+++ cmb.go
@@ -4,13 +4,16 @@ import (
 	"bytes"
 	"encoding/base64"
 	"fmt"
+	"github.com/ledongthuc/pdf"
 	"io"
 	"io/ioutil"
 	"log"
+	"math"
 	"mime"
 	"mime/multipart"
 	"net/mail"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -20,6 +23,23 @@ import (
 	"golang.org/x/text/encoding/simplifiedchinese"
 )
 
+// emailBillOrder 邮件订单
+type emailBillOrder struct {
+	Name   string `json:"name"`
+	Time   string `json:"time"`
+	Amount string `json:"amount"`
+}
+
+// fileBillOrder 出账单订单, 交易日,记账日,交易摘要,人民币金额,卡号末四位,交易地金额
+type fileBillOrder struct {
+	TradingDate   string `json:"tradingDate"`
+	CreditDate    string `json:"creditDate"`
+	Name          string `json:"name"`
+	Amount        string `json:"amount"`
+	TailNumber    string `json:"tailNumber"`
+	TradingAmount string `json:"tradingAmount"`
+}
+
 var (
 	// 07:53:41
 	timeRe = regexp.MustCompile(`(\d{2}:\d{2}:\d{2})`)
@@ -27,8 +47,8 @@ var (
 	orderRe = regexp.MustCompile(`(?m)([\d:]+)人民币([  \d.,]+)(.*)`)
 )
 
-func cmb(cfg fetchConfig) ([]billOrder, error) {
-	var orders = make([]billOrder, 0)
+func emailParseCmb(cfg fetchConfig) ([]emailBillOrder, error) {
+	var orders = make([]emailBillOrder, 0)
 	c, err := client.DialTLS("imap.exmail.qq.com:993", nil)
 	if err != nil {
 		return orders, fmt.Errorf("dial imap server error: %s", err.Error())
@@ -148,7 +168,7 @@ func cmb(cfg fetchConfig) ([]billOrder, error) {
 							amountStr := strings.TrimSpace(strings.ReplaceAll(match[2], ",", ""))
 							name := strings.TrimSpace(match[3])
 
-							orders = append(orders, billOrder{Name: name, Time: dateStr + " " + timeStr, Amount: amountStr})
+							orders = append(orders, emailBillOrder{Name: name, Time: dateStr + " " + timeStr, Amount: amountStr})
 						}
 					}
 				}
@@ -160,5 +180,95 @@ func cmb(cfg fetchConfig) ([]billOrder, error) {
 	if err := <-done; err != nil {
 		return orders, err
 	}
+	return orders, nil
+}
+
+func fileParseCmb(file string) ([]fileBillOrder, error) {
+	var lines = make([][]string, 0)
+	f, r, err := pdf.Open(file)
+	if err != nil {
+		return nil, fmt.Errorf("fail to open file: %s", file)
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+
+	var year int
+	yearRe := regexp.MustCompile(`\d{4}`)
+	blankRe := regexp.MustCompile(`\s+`)
+	// the x coordinate
+	var x float64
+	// the y coordinate
+	var y float64
+	var line string
+	pages := r.NumPage()
+	for i := 1; i <= pages; i++ {
+		p := r.Page(i)
+		texts := p.Content().Text
+		// a text is just a word
+		for _, text := range texts {
+			// determine new line
+			if math.Abs(text.Y-y) > 1 {
+				// find year
+				if year == 0 && yearRe.FindString(line) != "" {
+					year, _ = strconv.Atoi(yearRe.FindString(line))
+				}
+				// stop if reach end of file
+				if strings.TrimSpace(line) == "本期账单金额" {
+					break
+				}
+				if len(strings.Split(line, "|")) == 6 {
+					// 1,100 to 1100
+					line = strings.ReplaceAll(line, ",", "")
+					// replace blank
+					line = blankRe.ReplaceAllString(line, "")
+					lines = append(lines, strings.Split(line, "|"))
+				}
+				x, y, line = 0, text.Y, ""
+			}
+			// determine split in one line
+			if x != 0 && math.Abs(text.X-x) > 10 {
+				line += "|"
+			}
+			x = text.X
+			line += text.S
+		}
+	}
+	// format data
+	// determine if date cross year (transaction date increase in order)
+	// skip title
+	firstMon, _ := strconv.Atoi(strings.Split(lines[1][0], "/")[0])
+	lastMon, _ := strconv.Atoi(strings.Split(lines[len(lines)-1][0], "/")[0])
+	crossYear := (firstMon - lastMon) > 0
+
+	var orders []fileBillOrder
+
+	var max int
+	for idx, l := range lines {
+		// skip title
+		if idx == 0 {
+			continue
+		}
+		// date format
+		mon, _ := strconv.Atoi(strings.Split(l[0], "/")[0])
+		if crossYear && mon >= max {
+			max = mon
+			l[0] = strconv.Itoa(year-1) + "-" + strings.ReplaceAll(l[0], "/", "-")
+			l[4] = strconv.Itoa(year-1) + "-" + strings.ReplaceAll(l[4], "/", "-")
+		} else {
+			l[0] = strconv.Itoa(year) + "-" + strings.ReplaceAll(l[0], "/", "-")
+			l[4] = strconv.Itoa(year) + "-" + strings.ReplaceAll(l[4], "/", "-")
+		}
+		
+		orders = append(orders, fileBillOrder{
+			TradingDate:   l[0],
+			Name:          l[1],
+			Amount:        l[2],
+			TailNumber:    l[3],
+			CreditDate:    l[4],
+			TradingAmount: l[5],
+		})
+	}
+
 	return orders, nil
 }
